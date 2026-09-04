0ff6d408356a7b13b5b9f83614cbd5a1bf536d9d
Osama <osamamohammed0217@gmail.com>
2026-09-05 01:44:51 +0300

    Change  Usernames

--- lib/core/urls.dart
+++ lib/core/urls.dart
@@ -2,16 +2,16 @@ class AppUrls {
   AppUrls._();
 
   static const github = 'https://github.com/Osamamohammed050';
-  static const linkedin = 'https://www.linkedin.com/in/ousa0217/';
-  static const facebook = 'https://www.facebook.com/share/1aZMJRceJY/';
-  static const instagram = 'https://www.instagram.com/ousa.0217/';
-  static const threads = 'https://threads.net/@ousa.0217';
-  static const twitter = 'https://twitter.com/ousa0217';
-  static const tiktok = 'https://www.tiktok.com/@ousa.0217';
+  static const linkedin = 'https://www.linkedin.com/in/elous1/';
+  static const facebook = 'https://www.facebook.com/share/19D7BWbmYh/';
+  static const instagram = 'https://www.instagram.com/el_ous1/';
+  static const threads = 'https://threads.net/@el_ous1';
+  static const twitter = 'https://twitter.com/el_ous1';
+  static const tiktok = 'https://www.tiktok.com/el_ous1';
   static const telegram = 'https://t.me/ousa0217';
   static const whatsapp = 'https://wa.me/01123511467';
-  static const discord = 'https://discord.com/users/ousa.0217';
-  static const snapchat = 'https://www.snapchat.com/add/ousa.0217';
+  static const discord = 'https://discord.com/users/el_ous1';
+  static const snapchat = 'https://www.snapchat.com/add/elous1';
   static const instaPay = 'https://ipn.eg/S/osama0217/instapay/3VzLne';
   static const email = 'mailto:ous17102006@gmail.com';
   static const saraha = 'https://sarhne.sarahah.pro/osamaous';
